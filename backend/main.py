a20de8aad82d68066f67efe2d5cca3b24abdf888
Aaron Van Curen <99290929+aaronvancuren@users.noreply.github.com>
2024-09-24 12:16:41 -0400

    finalized backend framework

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
-import openai
 import os
-
+import openai
+from openai import OpenAI
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from fastapi import FastAPI, HTTPException
@@ -9,37 +9,64 @@ from fastapi.middleware.cors import CORSMiddleware
 # Load the environment variables from .env
 load_dotenv()
 
-# Load your OpenAI API key from an environment variable or secret management service
-openai.api_key = os.getenv("OPENAI_API_KEY")
+# Set up default values for OpenAI client
+client = OpenAI(
+    api_key = os.getenv("OPENAI_API_KEY"),
+    organization= os.getenv("OPENAI_ORGANIZATION"),
+    project= os.getenv("OPENAI_PROJECT")
+)
 
+# Set up FastAPI settings
 app = FastAPI()
 
 # Allow CORS for local development (adjust origins as needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React app runs on port 3000
+    allow_origins=[os.getenv("ORIGINS")],  # React app runs on port 3000
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 class Message(BaseModel):
+    """OpenAI message model"""
     content: str
 
-@app.post("/chat/")
+@app.post("/chat/", tags=["Chatbot"])
 async def chat(message: Message):
+    """OpenAI chat endpoint
+    Args:
+        message: User chat input
+    
+    Returns:
+        OpenAI response
+    """
     try:
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # or "gpt-4" if you have access
+        response = client.chat.completions.create(
             messages=[
-                {"role": "user", "content": message.content}
+                {
+                    "role": "user",
+                    "content": message.content
+                }
             ],
-            max_tokens=150,
+            model=os.getenv("OPENAI_MODEL"),
+            max_completion_tokens=os.getenv("OPENAI_MAX_COMPLETION_TOKENS"),
             n=1,
             stop=None,
             temperature=0.7,
+            # TODO add user field once user authentication is figured out
         )
         reply = response.choices[0].message.content.strip()
         return {"reply": reply}
+    except openai.APIConnectionError as e:
+        print("The server could not be reached")
+        print(e.__cause__)  # an underlying Exception, likely raised within httpx.
+    except openai.RateLimitError as e:
+        print("A 429 status code was received; we should back off a bit.")
+        print(e.status_code)
+    except openai.APIStatusError as e:
+        print("Another non-200-range status code was received")
+        print(e.status_code)
+        print(e.response)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e)) from e
