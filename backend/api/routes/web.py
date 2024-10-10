3030cba9f56dd5bce0b66c449c123108f9b222b3
The-Architect01 <bircng01@pfw.edu>
2024-10-10 00:29:53 -0400

    Worked on templates

--- backend/api/routes/web.py
+++ backend/api/routes/web.py
@@ -1,4 +1,9 @@
-from ...core import page_templates, error_templates
+from starlette.responses import RedirectResponse
+
+from ... core import page_templates, error_templates
+
+from .. authentication.Microsoft.auth_config import *
+
 #region Endpoint Pages
 
 # Note: Any page will need to follow this format: 
@@ -6,12 +11,13 @@ from ...core import page_templates, error_templates
 #       return templates.TemplateResponse(request, <PATH>)
 
 # The homepage
+
 async def homepage(request):
-    return page_templates.TemplateResponse(request, 'index.html')
+    return page_templates.TemplateResponse(request, 'index.html', loggedin=ACCOUNT_LOGGED_IN)
 
 # The Chat Page
 async def chatpage(request):
-    return page_templates.TemplateResponse(request, 'chat.html')
+    return page_templates.TemplateResponse(request, 'chat.html', loggedin=ACCOUNT_LOGGED_IN)
 
 #endregion
 
