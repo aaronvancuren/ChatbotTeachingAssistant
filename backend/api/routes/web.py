77b2d84c8b8470ec8f6e4c00854452571e607562
Aaron Van Curen <99290929+aaronvancuren@users.noreply.github.com>
2024-10-11 12:15:40 -0400

    implemented apirouter

--- backend/api/routes/web.py
+++ backend/api/routes/web.py
@@ -1,5 +1,8 @@
 """TODO: update docstring"""
 from backend.core import page_templates
+from fastapi import APIRouter
+
+webrouter = APIRouter()
 #region Endpoint Pages
 
 # Note: Any page will need to follow this format:
@@ -7,13 +10,13 @@ from backend.core import page_templates
 #       return templates.TemplateResponse(request, <PATH>)
 
 # The homepage
-@app.get("/")
+@webrouter.get("/")
 async def homepage(request):
     """TODO: update docstring"""
     return page_templates.TemplateResponse(request, 'index.html')
 
 # The Chat Page
-@app.get("/chat")
+@webrouter.get("/chat")
 async def chatpage(request):
     """TODO: update docstring"""
     return page_templates.TemplateResponse(request, 'chat.html')
