95b8077c842c2145c18468243380a9d89c4eb0eb
Aaron Van Curen <99290929+aaronvancuren@users.noreply.github.com>
2024-10-18 10:39:17 -0400

    corrected merge from main

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 """Initializing the FastAPI application"""
 
 import os
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
