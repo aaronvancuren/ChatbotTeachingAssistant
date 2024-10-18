7505842068abf5593a012ddf3f60c98476fe7e42
Aaron Van Curen <99290929+aaronvancuren@users.noreply.github.com>
2024-10-18 15:06:03 -0400

    added conversation context

--- backend/api/routes/openai.py
+++ backend/api/routes/openai.py
@@ -8,6 +8,9 @@ from backend.models.openai import *
 client = OpenAI()
 openai_router = APIRouter()
 
+# Maintains a single conversation for a single user
+conversation = []
+
 @openai_router.post("/ask", tags=["Chatbot"])
 async def chat(message: Message):
     """OpenAI chat endpoint for communciating with the specified OpenAI model
@@ -18,11 +21,15 @@ async def chat(message: Message):
         OpenAI response
     """
     try:
+        # Adds the user input to the conversation
+        conversation.append(message.content)
+        
+        # Sends the entire conversation to ChatGPT
         response = client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
-                    "content": message.content
+                    "content": conversation
                 }
             ],
             model=os.getenv("OPENAI_MODEL"),
@@ -33,6 +40,11 @@ async def chat(message: Message):
             # TODO add user field once user authentication is figured out
         )
         reply = response.choices[0].message.content.strip()
+        
+        # Adds the ChatGPT response to the conversation
+        conversation.append(reply)
+        
+        # Returns the ChatGPT response to the frontend to display
         return {"reply": reply}
     except _exceptions.APIConnectionError as e:
         print("The server could not be reached")
