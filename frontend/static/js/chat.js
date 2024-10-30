d089bf8288f052efe496e2e2b8ffdbb6862a2f24
Neal Birchfield <84486562+The-Architect01@users.noreply.github.com>
2024-10-30 10:26:53 -0400

    Select Model for Queries (#73)
    * Updated UI issues
    
    * Extracted sidebar to a template
    
    * Implemented model selection.
    
    * Fixed issues with naming

--- frontend/static/js/chat.js
+++ frontend/static/js/chat.js
@@ -6,7 +6,8 @@ function AskQuestion() {
         url: "/ask",
         data: JSON.stringify({
             chatbot: chatbot,
-            user_content: document.getElementById("question").value
+            user_content: document.getElementById("question").value,
+            teaching_assistant: document.getElementById('teaching_assistant').value
         }),        
         headers: {
             "X-victor-uid": "DEMO-1234", //Replace with UUID Cookie
@@ -17,8 +18,8 @@ function AskQuestion() {
         },
         success: function(data) {
             chatbot = JSON.parse(data);
-            createChatBubble(document.getElementById("question").value, ["btm-right", "user"]);
-            createChatBubble(chatbot[chatbot.length - 1][1], ["btm-left", "victor"]);
+            createChatBubble(document.getElementById("question").value, ["btm-right", "student"]);
+            createChatBubble(chatbot[chatbot.length - 1][1], ["btm-left", "teaching_assistant"]);
             document.getElementById("question").value = "";
         },
         statusCode:  {
@@ -35,8 +36,25 @@ function AskQuestion() {
 function createChatBubble(dialogue, classes){
     wrapper = document.getElementById("conversation");
     
+    containerWrapper = document.createElement("div");
+    containerWrapper.classList.add('talk-bubble');
+    chatWrapper = document.createElement("p");
+    
+    if (classes.includes('teaching_assistant')) {
+        chatWrapper.innerText = `${document.getElementById('teaching_assistant').options[document.getElementById('teaching_assistant').selectedIndex].text} says...`;
+        containerWrapper.classList.add('left');
+        chatWrapper.classList.add('left')
+    } else {
+        chatWrapper.innerText = 'You asked...';
+        containerWrapper.classList.add('right');
+        chatWrapper.classList.add('right')
+    }
+
+    chatWrapper.classList.add('text-from')
+    containerWrapper.appendChild(chatWrapper);
+
     container = document.createElement("div");
-    container.classList.add("talk-bubble", "tri-right", ...classes);
+    container.classList.add("tri-right", ...classes);
         
     displayContainer = document.createElement("div");
     displayContainer.classList.add("talktext");
@@ -46,5 +64,13 @@ function createChatBubble(dialogue, classes){
     
     displayContainer.appendChild(dialogueContainer);
     container.appendChild(displayContainer);
-    wrapper.appendChild(container);
+    containerWrapper.appendChild(container);
+    wrapper.appendChild(containerWrapper);
+}
+
+function updateDisclaimer(){
+    let target = document.getElementById('Disclaimer')
+    let newVal = document.getElementById('teaching_assistant').options[document.getElementById('teaching_assistant').selectedIndex].text;
+    target.innerText = target.innerText.replace(/[^\s]*/, newVal);
+    document.getElementById('teaching_assistant').title = document.getElementById('TAs').options[document.getElementById('teaching_assistant').selectedIndex].title;
 }
