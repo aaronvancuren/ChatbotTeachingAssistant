56115f1849b0655c2de1959ef92fd8e403725969
The-Architect01 <bircng01@pfw.edu>
2024-11-15 13:28:19 -0500

    Removed demo material

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,6 @@ from starlette.middleware.sessions import SessionMiddleware
 from backend.api.authentication.Microsoft import AUTHENTICATION_SERVER
 from backend.api.routes.web import web_router
 from backend.api.routes.openai import openai_router
-from backend.api.routes.upload import upload_router
 
 # Set up FastAPI settings
 app = FastAPI()
@@ -18,7 +17,6 @@ app = FastAPI()
 app.include_router(web_router)
 app.include_router(openai_router)
 app.include_router(AUTHENTICATION_SERVER.router)
-app.include_router(upload_router)
 
 app.mount('/static', StaticFiles(directory='frontend/static'), name='static')
 
