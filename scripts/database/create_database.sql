26c36890e2e0c161c03db76ebf1ed78bc9e74478
aaronvancuren <99290929+aaronvancuren@users.noreply.github.com>
2025-02-13 21:43:06 -0500

    updated owner for local server

--- scripts/database/create_database.sql
+++ scripts/database/create_database.sql
@@ -1,8 +1,9 @@
 -- Database: chatbot
+-- OWNER depends on the default local server. Update as necessary.
 
 CREATE DATABASE chatbot
     WITH
-    OWNER = cloudsqlsuperuser
+    OWNER = postgres
     ENCODING = 'UTF8'
     LC_COLLATE = default
     LC_CTYPE = default
