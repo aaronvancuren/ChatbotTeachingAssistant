19bd45468926c826647084442771039bb9f1038e
Neal Birchfield <84486562+grumpy-lions@users.noreply.github.com>
2025-03-20 15:17:15 -0400

    64 - profile page (#120)
    * bug fix
    
    * Possible fix for the chat display issue
    
    * Prevent HTML injection
    
    * Fixed graphic issue with HTML cleaning
    
    * Added rough draft
    
    * Fixed Commit

---
+++ frontend/static/js/profile.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict'
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+        new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})()
+
+document.querySelectorAll('#nav-tab>[data-bs-toggle="tab"]').forEach(el => {
+    el.addEventListener('shown.bs.tab', () => {
+        const target = el.getAttribute('data-bs-target')
+        const scrollElem = document.querySelector(`${target} [data-bs-spy="scroll"]`)
+        bootstrap.ScrollSpy.getOrCreateInstance(scrollElem).refresh()
+    })
+})
