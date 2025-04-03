90cfd83ccacf303fc9ce2002ead5218657d5a4a1
Aaron Van Curen <99290929+aaronvancuren@users.noreply.github.com>
2025-04-03 02:39:54 -0400

    updated sql scripts to cascade when fk is updated

--- scripts/database/create_table_users.sql
+++ scripts/database/create_table_users.sql
@@ -12,9 +12,10 @@ CREATE TABLE IF NOT EXISTS public.users
     archived_at timestamp with time zone,
     CONSTRAINT users_pkey PRIMARY KEY (email),
     CONSTRAINT users_email_key UNIQUE (email),
+    CONSTRAINT users_id UNIQUE (id),
     CONSTRAINT users_archived_by_fkey FOREIGN KEY (archived_by)
         REFERENCES public.users (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
+        ON UPDATE CASCADE
         ON DELETE RESTRICT
 )
 
