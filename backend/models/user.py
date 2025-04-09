1c68a27178b4acb2d9f9dc24f55fb0d75cced71b
WCeresa <wrightceresa@gmial.com>
2025-04-09 12:20:20 -0400

    Adds get_course method for retrieving user courses

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,6 +1,8 @@
 import uuid
 from backend.models import UserBase, CourseBase, Message, Role
 from psycopg2.extras import RealDictRow
+from backend.models.subject import Subject
+
 
 class User(UserBase):
     courses: list[CourseBase] = []
@@ -9,8 +11,37 @@ class User(UserBase):
         super().__init__(id=row['id'], display_name=row['display_name'], email=row['email'], role=Role[row['role']])
         
     # Get user courses
-    def get_courses() -> list[CourseBase]:
-        pass
+    def get_courses(self) -> list[CourseBase]:
+        from backend.database.postgres import read_courses_for_user
+        from backend.models.course import Course
+        # Query the DB to retrieve classes for this user
+        class_list = read_courses_for_user(str(self.id))
+
+        # Convert each row/dict into a Course
+        courses = []
+        for class_info in class_list:
+            raw_subject = class_info["subject"]
+            subject_val = Subject[raw_subject.upper()]
+
+            c = Course(
+                id=class_info["id"],
+                instructor_id=class_info["instructor_id"],
+                display_name=class_info["display_name"],
+                subject=subject_val,
+                course_number=class_info["course_number"],
+                section_number=class_info["section_number"],
+                title=class_info["title"],
+                model=class_info["model"],
+                prompt=class_info["prompt"],
+                documents_path=class_info["documents_path"],
+                image_path=class_info["image_path"],
+                students=[]
+            )
+
+            c.students = c.get_students()
+
+            courses.append(c)
+        return courses
     
     # Get user conversations by course
     def get_conversations(self, course_id: uuid) -> list[Message]:
