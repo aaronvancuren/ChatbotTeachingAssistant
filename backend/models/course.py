265b9454c53c2e708bc54ad4561b55b8028cd81b
CarterBesson <122425573+CarterBesson@users.noreply.github.com>
2025-04-14 11:58:58 -0400

    122 add students to class (#139)

--- backend/models/course.py
+++ backend/models/course.py
@@ -1,11 +1,12 @@
 from backend.models import CourseBase, UserBase
+from backend.models.user import User
 
 class Course(CourseBase):
     students: list[UserBase]
     
-    # Get a list of students
-    def get_students() -> list[UserBase]:
-        pass
+    def get_students(self):
+        from backend.database.postgres import read_students_for_course
+        self.students = read_students_for_course(str(self.id))
     
     # Get a list of documents
     def get_documents(): # What is the type?
